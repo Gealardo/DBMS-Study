9825612193e32d98ff4590d2eb820005f61e47f9
Gealardo <ayunverma902@gmail.com>
2026-08-26 00:56:11 +0530

    Insert new customer records into customers table
    Added two new customer records to the database.

--- SQL/03_insert_update_delete.sql
+++ SQL/03_insert_update_delete.sql
@@ -19,3 +19,35 @@ id          first_name                                         country          
 6           Gea                                                India                                              600
 7           Ardo                                               USA                                                NULL
 */
+
+--Also Works
+INSERT INTO customers
+VALUES (8,'Gealardo','India',700) --added this row 
+
+-- can skip null only and with fist method       
+INSERT INTO customers ( id,first_name)
+VALUES (9,'Ayunji') --added this row 
+/*
+2            John                                              USA                                                900
+3           Georg                                              UK                                                 750
+4           Martin                                             Germany                                            500
+5           Peter                                              USA                                                0
+6           Gea                                                India                                              600
+7           Ardo                                               USA                                                NULL
+8           Gealardo                                           India                                              700
+
+(8 rows affected)
+
+id          first_name                                         country                                            score
+----------- -------------------------------------------------- -------------------------------------------------- -----------
+1           Maria                                              Germany                                            350
+2            John                                              USA                                                900
+3           Georg                                              UK                                                 750
+4           Martin                                             Germany                                            500
+5           Peter                                              USA                                                0
+6           Gea                                                India                                              600
+7           Ardo                                               USA                                                NULL
+8           Gealardo                                           India                                              700
+9           Ayunji                                             NULL                                               NULL
+
+*/
